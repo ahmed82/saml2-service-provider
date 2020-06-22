af604eed0235db83080cf00c2889787ef613e221
Ahmed AlSalih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-06-22 17:43:08 -0400

    init SAML SP

---
+++ src/main/java/com/sp/saml/SamlSpServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.sp.saml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlSpServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlSpServiceProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/saml/SamlSpServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlSpServiceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
